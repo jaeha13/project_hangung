448c785ee3882b6640c09cb2b02b18e12064416d
jaeha13 <hajaeyeon13@gmail.com>
2021-08-04 17:33:23 +0900

    gung setting

--- project_hangung/project_hangung/urls.py
+++ project_hangung/project_hangung/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mainapp/',include('mainapp.urls'))
+    path('mainapp/',include('mainapp.urls')),
+    path('gungapp/', include('gungapp.urls')),
 ]
